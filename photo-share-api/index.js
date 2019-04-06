c50b0478a74b1872f48f7b8589b42f0100acab9c
Richard Otvos <>
2019-04-06 20:11:59 +0200

    Adds basic photo site schema

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -1,28 +1,120 @@
 const { ApolloServer } = require('apollo-server');
 
-let photos = [];
+var _id = 0;
+//sample data
+var users = [
+    { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+    { "githubLogin": "gPlake", "name": "Glen Plake" },
+    { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
+]
 
+var photos = [
+    {
+        "id": "1",
+        "name": "Dropping the Heart Chute",
+        "description": "The heart chute is one of my favorite chutes",
+        "category": "ACTION",
+        "githubUser": "gPlake"
+    },
+    {
+        "id": "2",
+        "name": "Enjoying the sunshine",
+        "category": "SELFIE",
+        "githubUser": "sSchmidt"
+    },
+    {
+        id: "3",
+        "name": "Gunbarrel 25",
+        "description": "25 laps on gunbarrel today",
+        "category": "LANDSCAPE",
+        "githubUser": "sSchmidt"
+    }
+]
+
+var tags = [
+    { "photoID": "1", "userID": "gPlake" },
+    { "photoID": "2", "userID": "sSchmidt" },
+    { "photoID": "2", "userID": "mHattrup" },
+    { "photoID": "2", "userID": "gPlake" }
+]
+
+//schema def
 const typeDefs = `
+    enum PhotoCategory{
+        SELFIE
+        PORTRAIT
+        ACTION
+        LANDSCAPE
+        GRAPHIC
+    }
+
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+        category: PhotoCategory!
+        postedBy: User!
+        taggedUsers: [User!]!
+    }
+
+    type User {
+        githubLogin: ID!
+        name: String
+        avatar: String
+        postedPhotos: [Photo!]!
+        inPhotos: [Photo!]!
+    }
+    
     type Query {
         totalPhotos: Int!
+        allPhotos: [Photo!]!
+    }
+
+    input PostPhotoInput {
+        name: String!
+        category: PhotoCategory=PORTRAIT
+        description: String
     }
 
     type Mutation{
-        postPhoto(name: String! description: String): Boolean!
+        postPhoto(input: PostPhotoInput!): Photo!
     }
 `;
 
 const resolvers = {
     Query: {
-        totalPhotos: () => photos.length
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos
     },
     Mutation: {
-        postPhoto: (parent, args) => {
-            console.log(parent);
-            console.log(args);
-            photos.push(args);
-            return true;
+        postPhoto: (parent, { input: photoInput }) => {
+            const newPhoto = {
+                id: _id,
+                ...photoInput
+            }
+
+            _id += 1;
+            photos.push(newPhoto);
+            return newPhoto;
         }
+    },
+    Photo: {
+        url: (parent) => `http://foobarsite.com/img/img-${parent.id}.webp`,
+        postedBy: (parent) => {
+            return users.find(user => user.githubLogin === parent.githubUser)
+        },
+        taggedUsers: (parent) => tags.filter(tag => tag.photoID === parent.id)
+            .map(tag => tag.userID)
+            .map(userID => users.find(u => u.githubLogin === userID))
+    },
+    User: {
+        postedPhotos: (parent) => {
+            return photos.filter(photo => photo.githubUser === parent.githubLogin)
+        },
+        inPhotos: (parent) => tags.filter(tag => tag.userID === parent.id)
+            .map(tag => tag.photoID)
+            .map(photoID => photos.find(p => p.id === photoId))
     }
 };
 
